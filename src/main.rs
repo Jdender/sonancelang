0f73be0f4abfc1c13f788103e46c725c71c60bc0
Jdender~ <jdenderplays@gmail.com>
2020-05-30 11:52:49 -0400

    Readd precedence

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::{env::current_dir, fs::File, io::prelude::*};
 
 fn main() {
     let program = r"
-        let t = ((22. * 44.) + 66.);
-        let a = (12 * (1 + 5));
-        (32 + 64)
+        let t = 22.0 * 44.0 + 66.0;
+        let a = 12 * 1 + 5;
+        32 + 64
     ";
 
     let compiled = compile(program).unwrap();
