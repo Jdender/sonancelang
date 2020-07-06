39a40a56d43e9c3c9f462adfe2f3f2e791770b09
Jdender~ <jdenderplays@gmail.com>
2020-07-06 01:31:38 -0400

    Let's start from the begining but actualy get somewhere this time

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate lalrpop_util;
 
-lalrpop_mod!(pub parser);
+lalrpop_mod!(pub grammar);
 
 pub mod parse;
 
--- src/test.rs
+++ src/test.rs
@@ -1,111 +1,12 @@
-use crate::parse::*;
-use crate::parser::*;
-
-fn ident(input: &str) -> Identifier {
-    Identifier(input.to_string())
+fn compile(_input: &str) {
+    todo!();
 }
 
 #[test]
-fn identifier_test() {
-    let parser = IdentifierParser::new();
-
-    assert_eq!(parser.parse("foobar").unwrap(), ident("foobar"));
-    assert_eq!(parser.parse("asa_adw").unwrap(), ident("asa_adw"));
-    assert_eq!(parser.parse("a1234").unwrap(), ident("a1234"));
-
-    assert!(parser.parse("123asa").is_err());
-    assert!(parser.parse("1").is_err());
-    assert!(parser.parse("[]+=-").is_err());
-}
-
-#[test]
-fn function_test() {
-    let parser = FunctionItemParser::new();
-
-    assert_eq!(
-        parser.parse("func foobar() {}").unwrap(),
-        FunctionItem {
-            name: ident("foobar"),
-            arguments: vec![],
-            return_type: None,
-            body: Block {
-                body: vec![],
-                trailing: None
-            },
+fn return_int_test() {
+    compile(r"
+        func main() -> U32 {
+            return 1234;
         }
-    );
-
-    assert_eq!(
-        parser.parse("func foobar() -> Type {}").unwrap(),
-        FunctionItem {
-            name: ident("foobar"),
-            arguments: vec![],
-            return_type: Some(Type {
-                name: ident("Type"),
-                arguments: vec![]
-            }),
-            body: Block {
-                body: vec![],
-                trailing: None
-            },
-        }
-    );
-}
-
-#[test]
-fn arguments_test() {
-    let parser = ArgumentsParser::new();
-
-    assert_eq!(
-        parser.parse("(a: A, b: B)").unwrap(),
-        vec![
-            Argument {
-                pattern: Pattern::Identifier(ident("a")),
-                declared_type: Type {
-                    name: ident("A"),
-                    arguments: vec![]
-                },
-            },
-            Argument {
-                pattern: Pattern::Identifier(ident("b")),
-                declared_type: Type {
-                    name: ident("B"),
-                    arguments: vec![]
-                },
-            }
-        ]
-    );
-}
-
-#[test]
-fn type_test() {
-    let parser = TypeParser::new();
-
-    assert_eq!(
-        parser.parse("Foo").unwrap(),
-        Type {
-            name: ident("Foo"),
-            arguments: vec![]
-        }
-    );
-
-    assert_eq!(
-        parser.parse("Foo[Bar[Baz], Buz[]]").unwrap(),
-        Type {
-            name: ident("Foo"),
-            arguments: vec![
-                Type {
-                    name: ident("Bar"),
-                    arguments: vec![Type {
-                        name: ident("Baz"),
-                        arguments: vec![]
-                    }]
-                },
-                Type {
-                    name: ident("Buz"),
-                    arguments: vec![]
-                }
-            ]
-        }
-    );
+    ");
 }
