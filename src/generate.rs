554083a128ecb4b7e29952fdf51d69557f92b0ab
Jdender~ <jdenderplays@gmail.com>
2020-07-06 01:57:19 -0400

    Add the generate step

---
+++ src/generate.rs
@@ -0,0 +1,31 @@
+use crate::parse::*;
+use parity_wasm::{
+    builder::ModuleBuilder,
+    elements::{Instruction, Instructions, Module},
+};
+
+pub trait AstVisitor {
+    type Argument;
+    type Return;
+
+    fn visit_ast(&self, args: Self::Argument) -> Self::Return;
+}
+
+impl AstVisitor for File {
+    type Argument = ();
+    type Return = Module;
+
+    fn visit_ast(&self, (): Self::Argument) -> Self::Return {
+        ModuleBuilder::new()
+            .function()
+            .signature()
+            .return_type()
+            .i32()
+            .build()
+            .body()
+            .with_instructions(Instructions::new(vec![Instruction::I32Const(self.0)]))
+            .build()
+            .build()
+            .build()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,24 @@ extern crate lalrpop_util;
 
 lalrpop_mod!(pub grammar);
 
-pub mod parse;
-
 #[cfg(test)]
 mod test;
+
+pub mod generate;
+pub mod parse;
+
+use generate::AstVisitor;
+use parse::ParseError;
+
+use parity_wasm::elements::Module;
+
+pub enum CompilerError<'input> {
+    ParseError(ParseError<'input>),
+}
+
+pub fn compile(input: &str) -> Result<Module, CompilerError> {
+    Ok(grammar::FileParser::new()
+        .parse(input)
+        .map_err(CompilerError::ParseError)?
+        .visit_ast(()))
+}
