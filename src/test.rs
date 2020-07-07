d5111735257a13bc23378e227cc3b73e3af2f138
Jdender~ <jdenderplays@gmail.com>
2020-07-06 20:19:57 -0400

    Add test helpers

--- src/test.rs
+++ src/test.rs
@@ -1,15 +1,37 @@
 use crate::compile;
+use wasmer_runtime::{imports, instantiate, DynFunc, Value};
+
+fn compile_and_run(input: &str) -> i32 {
+    let import_object = imports! {};
+    let wasm = compile(input).unwrap().wasm.to_bytes().unwrap();
+    let instance = instantiate(&wasm, &import_object).unwrap();
+
+    let values = instance
+        .exports
+        .get::<DynFunc>("main")
+        .unwrap()
+        .call(&[])
+        .unwrap();
+
+    if let Value::I32(num) = values[0] {
+        num
+    } else {
+        Err(()).unwrap()
+    }
+}
+
+fn compile_run_eq(num: i32, input: &str) {
+    assert_eq!(num, compile_and_run(input));
+}
 
 #[test]
 fn return_int_test() {
-    let _ = compile(
+    compile_run_eq(
+        1234,
         r"
-        func main() -> I32 {
-            return 1234;
-        }
-    ",
-    )
-    .unwrap();
-
-    todo!("Figure out how to test this properly");
+             func main() -> I32 {
+                return 1234;
+            }
+        ",
+    );
 }
