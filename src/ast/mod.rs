2fd058e5aa71431f1ed39769a5e3dc984d64d149
Jdender~ <jdenderplays@gmail.com>
2020-10-09 21:17:15 -0400

    Remove src ™

--- src/ast/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod structure;
-
-lalrpop_mod!(#[allow(clippy::all)] pub grammar, "/ast/grammar.rs");
-
-pub use structure::*;
-
-pub fn ast_pass(input: &'_ str) -> Result<File, String> {
-    Ok(grammar::FileParser::new().parse(input).unwrap())
-}
